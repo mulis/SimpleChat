5dd06703f7e5b62bf58f726b48b33e0e1b21a695
Serhij Mulishov <serhij.mulishov@gmail.com>
2013-01-09 22:41:58 +0200

    0.6-snapshot

--- src/main/webapp/DivEditor/UI/UI.js
+++
@@ -1,2 +0,0 @@
-DivEditor.UI = function() {
-}
--- src/main/webapp/scripts/index.js
+++
@@ -1,9 +0,0 @@
-var c1, c2, c3;
-
-$("document").ready(function() {
-
-    c1 = new Chat("chat1", "http://localhost:8080/chat");
-    c2 = new Chat("chat2", "http://localhost:8080/chat");
-    c3 = new Chat("chat3", "http://localhost:8080/chat");
-
-});
